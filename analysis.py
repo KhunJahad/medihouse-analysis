6f4948c2ca1c13f084cb3f01bf2dccfd29ef4ae5
KhunJahad <Khungaur@gmail.com>
2021-11-16 14:53:47 +0530

    initial commit

--- analysis.py
+++ analysis.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
-st.title('This will have the analysis')
-st.title('More to be added')
+
+
+st.title('Medihouse Adherance Dashboard')
+st.markdown('The dashboard will visualize the Adherance Situation')
+st.sidebar.title('Analysis')
+st.sidebar.markdown('Select a Parameter:')
+
+parameter = ('Age','County','Ethencity','Race','Gender','City')
+select = st.sidebar.radio('',parameter)
